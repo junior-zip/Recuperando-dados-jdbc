9595525be6f3adee29217484a01058906abfd76d
Junior Teixeira <junior26bmx@gmail.com>
2022-10-19 12:13:09 -0300

    comentarios

--- src/application/Program.java
+++ src/application/Program.java
@@ -14,13 +14,15 @@ public class Program {
         ResultSet rs = null;
 
         try{
+            //criando uma conexao
             conn = DB.getConnection();
-
+            //instanciando conexaão statemant
             st = conn.createStatement();
-
+            //executando uma ação em sql. Ação para recuperar o dado
             rs = st.executeQuery("SELECT * FROM department");
 
             while(rs.next()){
+                //delimitando o id e o name para serem adicionados aqui
                 System.out.println(rs.getInt("Id") + ", " + rs.getString("Name"));
 
             }
